211ae1c4e6cdb4d3c97365b9a857594d7dc061da
hoborific <hoborific09@gmail.com>
2017-07-03 00:42:32 +1000

    Breeding Beta Test

--- main/java/com/hoborific/hobomod/HoboMod.java
+++ main/java/com/hoborific/hobomod/HoboMod.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class HoboMod
 {
     public static final String MODID = "hobomod";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 
     @Mod.Instance(MODID)
     public static HoboMod instance;
--- main/java/com/hoborific/hobomod/common/entity/EntityDyeableChicken.java
+++ main/java/com/hoborific/hobomod/common/entity/EntityDyeableChicken.java
@@ -1,12 +1,18 @@
 package com.hoborific.hobomod.common.entity;
 
 import com.google.common.collect.Maps;
+import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.IEntityLivingData;
+import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemDye;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
@@ -21,8 +27,22 @@ import java.util.Map;
 import static net.minecraft.entity.passive.EntitySheep.getRandomSheepColor;
 
 public class EntityDyeableChicken extends EntityChicken{
+
+    private final InventoryCrafting inventoryCrafting = new InventoryCrafting(new Container()
+    {
+        /**
+         * Determines whether supplied player can use this container
+         */
+        public boolean canInteractWith(EntityPlayer playerIn)
+        {
+            return false;
+        }
+    }, 2, 1);
+
     public EntityDyeableChicken(World worldIn){
         super(worldIn);
+        this.inventoryCrafting.setInventorySlotContents(0, new ItemStack(Items.DYE));
+        this.inventoryCrafting.setInventorySlotContents(1, new ItemStack(Items.DYE));
     }
     private static final Map<EnumDyeColor, float[]> DYE_TO_RGB = Maps.newEnumMap(EnumDyeColor.class);
     private static final DataParameter<Byte> DYE_COLOR = EntityDataManager.createKey(EntityDyeableChicken.class, DataSerializers.BYTE);
@@ -96,6 +116,36 @@ public class EntityDyeableChicken extends EntityChicken{
         super.readEntityFromNBT(compound);
         this.setFeatherColor(EnumDyeColor.byMetadata(compound.getByte("Color")));
     }
+    public EntityDyeableChicken createChild(EntityAgeable ageable)
+    {
+        EntityDyeableChicken entitydyeablechicken = (EntityDyeableChicken)ageable;
+        EntityDyeableChicken entitydyeablechicken1 = new EntityDyeableChicken(this.world);
+        entitydyeablechicken1.setFeatherColor(this.getDyeColorMixFromParents(this, entitydyeablechicken));
+        return entitydyeablechicken1;
+    }
+
+
+    private EnumDyeColor getDyeColorMixFromParents(EntityAnimal father, EntityAnimal mother)
+    {
+        int i = ((EntityDyeableChicken)father).getFeatherColor().getDyeDamage();
+        int j = ((EntityDyeableChicken)mother).getFeatherColor().getDyeDamage();
+        this.inventoryCrafting.getStackInSlot(0).setItemDamage(i);
+        this.inventoryCrafting.getStackInSlot(1).setItemDamage(j);
+        ItemStack itemstack = CraftingManager.getInstance().findMatchingRecipe(this.inventoryCrafting, ((EntityDyeableChicken)father).world);
+        int k;
+
+        if (itemstack.getItem() == Items.DYE)
+        {
+            k = itemstack.getMetadata();
+        }
+        else
+        {
+            k = this.world.rand.nextBoolean() ? i : j;
+        }
+
+        return EnumDyeColor.byDyeDamage(k);
+    }
+
 }
 
 
